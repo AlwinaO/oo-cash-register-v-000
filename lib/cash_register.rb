6297f5f5e44215693ff69d61ab42d1d72d53150a
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-03-09 02:40:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,10 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    @items << title
+    quantity.times do  
+      @items << title
+
+    end
 
   end
 
